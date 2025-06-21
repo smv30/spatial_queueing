f2d9660d846c5c8f0c38a768f98a744088d122ef
smv30 <sushil.varma30@gmail.com>
2025-06-20 19:55:39 -0700

    Generate Dataset Changes

--- uniform_dataset/generate_data.py
+++ uniform_dataset/generate_data.py
@@ -14,8 +14,6 @@ def generate_trips(sim_duration_min, arrival_rate_pmin, data_dir, data_no):
         "end_lat": [],
         "end_lon": []
     })
-    seed = 2023
-    SimMetaData.random_seed_gen = np.random.default_rng(seed)
     while curr_time_min <= sim_duration_min:
         start_lat = SimMetaData.random_seed_gen.uniform(0, SimMetaData.max_lat)
         start_lon = SimMetaData.random_seed_gen.uniform(0, SimMetaData.max_lon)
