1acb9eb5c8bdd09ab0553e66fbc47899190d75c0
Sushil M Varma <65252164+smv30@users.noreply.github.com>
2025-06-20 20:08:38 -0700

    Update generate_data.py

--- uniform_dataset/generate_data.py
+++ uniform_dataset/generate_data.py
@@ -43,8 +43,8 @@ if __name__ == "__main__":
     data_dir = os.path.join(curr_dir, "data")
     if not os.path.isdir(data_dir):
         os.makedirs(data_dir)
-    for input_arrival_rate_pmin in tqdm([5, 10, 20, 40, 80, 160, 320], desc="Generating Datasets"):
-        for data_no in [1, 2, 3, 4, 5]:
+    for input_arrival_rate_pmin in [5, 10, 20, 40, 80, 160, 320]:
+        for data_no in tqdm([1, 2, 3, 4, 5], desc=f"Generating Datasets for Arrival Rate {input_arrival_rate_pmin} min"):
             generate_trips(arrival_rate_pmin=input_arrival_rate_pmin,
                         sim_duration_min=1000,
                         data_dir= data_dir,
